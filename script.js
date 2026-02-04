40c73b73230fe22201dbe852695cbfa8b81452a7
MindViper325 <leo.marmayou@gmail.com>
2026-02-04 14:41:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
+
+document.getElementById("link-home").addEventListener("click", () => {
+  home.style.display = "block";
+  about.style.display = "none";
+  contact.style.display = "none";
+});
+
+document.getElementById("link-about").addEventListener("click", () => {
+  home.style.display = "none";
+  about.style.display = "block";
+  contact.style.display = "none";
+});
+
+document.getElementById("link-contact").addEventListener("click", () => {
+  home.style.display = "none";
+  about.style.display = "none";
+  contact.style.display = "block";
+});
+
 const button = document.getElementById("btn");
 const message = document.getElementById("message");
 
 button.addEventListener("click", () => {
-  message.textContent = "Bravo 🎉 Tu viens d'utiliser JavaScript !";
+  message.textContent = "Bravo 🎉 JavaScript fonctionne !";
 });
