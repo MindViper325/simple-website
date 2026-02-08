7066042432f8d908ca25a51c756cb63035ef993e
MindViper325 <leo.marmayou@gmail.com>
2026-02-08 17:17:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-const home = document.getElementById("home");
-const about = document.getElementById("about");
-const contact = document.getElementById("contact");
+const sections = document.querySelectorAll(".section");
+
+function showSection(id) {
+  sections.forEach(section => {
+    section.classList.remove("active");
+  });
+  document.getElementById(id).classList.add("active");
+}
 
 document.getElementById("link-home").addEventListener("click", () => {
-  home.style.display = "block";
-  about.style.display = "none";
-  contact.style.display = "none";
+  showSection("home");
 });
 
 document.getElementById("link-about").addEventListener("click", () => {
-  home.style.display = "none";
-  about.style.display = "block";
-  contact.style.display = "none";
+  showSection("about");
 });
 
-document.getElementById("link-contact").addEventListener("click", () => {
-  home.style.display = "none";
-  about.style.display = "none";
-  contact.style.display = "block";
+document.getElementById("link-games").addEventListener("click", () => {
+  showSection("games");
 });
 
+/* Bouton accueil */
 const button = document.getElementById("btn");
 const message = document.getElementById("message");
 
 button.addEventListener("click", () => {
-  message.textContent = "Bravo 🎉 JavaScript fonctionne !";
+  message.textContent = "Bienvenue dans le site de jeux 🎮";
+});
+
+/* MODE SOMBRE */
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
